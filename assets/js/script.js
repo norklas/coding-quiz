a4a1159107f2a8fc6eae0130139c1737b0590e0e
Nick Melanson <nickm890@gmail.com>
2022-02-13 01:57:51 -0500

    scrapped project, changing logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,54 @@
-// Element variables
-var timerEl = document.querySelector(".timer");
-var mainEl = document.querySelector(".container-main");
+// Global Variables
+
 var startBtn = document.querySelector(".start-button");
-var titleEl = document.querySelector(".title-h1");
-var startTextEl = document.querySelector(".start-text");
 
-// Creating Elements
-var btn1 = document.createElement("button");
-var btn2 = document.createElement("button");
-var btn3 = document.createElement("button");
-var btn4 = document.createElement("button");
+// Questions Array
 
-var answerSpan = document.createElement("span");
-
-// Adding class attributes for styling
-btn1.setAttribute("class", "answer-button");
-btn2.setAttribute("class", "answer-button");
-btn3.setAttribute("class", "answer-button");
-btn4.setAttribute("class", "answer-button");
-
-answerSpan.setAttribute("class", "answer-style");
-
-// Timer variables for countdown function
-var counter = 60;
-var timer = setInterval(countdown, 1000);
-
-// Function for timer
-function countdown() {
-  if (counter == 0) {
-    clearTimeout(timer);
-  } else {
-    timerEl.innerHTML = "Time: " + counter;
-    counter--;
-  }
-}
-
-// Start button event listener
-startBtn.addEventListener("click", function () {
-  questionOne();
-});
-
-// Function for first question
-
-function questionOne() {
-  titleEl.textContent = "Commonly used data types do NOT include:";
-  // Removing elements that will not be used
-  mainEl.removeChild(startBtn);
-  mainEl.removeChild(startTextEl);
-
-  // Appending buttons for answers
-  mainEl.appendChild(btn1);
-  mainEl.appendChild(btn2);
-  mainEl.appendChild(btn3);
-  mainEl.appendChild(btn4);
-
-  // Adding text content to list elements
-  btn1.innerText = "1. Strings";
-  btn2.innerText = "2. Booleans";
-  btn3.innerText = "3. Alerts";
-  btn4.innerText = "4. Numbers";
-
-  // Event Listeners for buttons
-  mainEl.addEventListener("click", function (event) {
-    if (event.target == btn1 || event.target == btn2 || event.target == btn4) {
-      mainEl.appendChild(answerSpan);
-      answerSpan.innerText = "Wrong!";
-    } else if (event.target == btn3) {
-      mainEl.appendChild(answerSpan);
-      answerSpan.innerText = "Correct!";
-    }
-  });
-}
+var questionsArr = [
+  {
+    question: "Commonly used data types do NOT include:",
+    answerChoices: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
+    correctAnswer: 2,
+  },
+  {
+    question: "The condition in an if/else statement is enclosed with _______",
+    answerChoices: [
+      "1. Quotes",
+      "2. Curly Brackets",
+      "3. Parenthesis",
+      "4. Square Brackets",
+    ],
+    correctAnswer: 2,
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ______.",
+    answerChoices: [
+      "1. Numbers and strings",
+      "2. Other Arrays",
+      "3. Booleans",
+      "4. All of the above",
+    ],
+    correctAnswer: 3,
+  },
+  {
+    question:
+      "Strings values must be enclosed within _______ when being assigned to variables.",
+    answerChoices: [
+      "1. Commas",
+      "2. Curly Brackets",
+      "3. Quotations",
+      "4. Parenthesis",
+    ],
+    correctAnswer: 2,
+  },
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answerChoices: [
+      "1. JavaScript",
+      "2. terminal/bash",
+      "3. for loop",
+      "4. console.log",
+    ],
+  },
+];
