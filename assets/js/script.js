0ef3a296cf26a1b055a7bf2cbc327e66dbdc7a3c
Nick Melanson <nickm890@gmail.com>
2022-02-13 13:06:52 -0500

    button eventlistener functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
 // Global Variables
 
 var startBtn = document.querySelector(".start-button");
+var homeDiv = document.querySelector(".home-container");
+var startBtn = document.querySelector(".start-button");
+var quizDiv = document.querySelector(".quiz-container");
+var questionTitle = document.querySelector(".question-title");
+var timerSpan = document.querySelector(".timer");
+var answerDiv = document.querySelector(".answers");
+var answerOne = document.querySelector("#answerOne");
+var answerTwo = document.querySelector("#answerTwo");
+var answerThree = document.querySelector("#answerThree");
+var answerFour = document.querySelector("#answerFour");
+var timer = 75;
+var timeStart = false;
+var timeRemain = true;
+
+// Question Index
+var i = 0;
 
 // Questions Array
 
@@ -52,3 +68,57 @@ var questionsArr = [
     ],
   },
 ];
+
+// Countdown logic
+
+var countdownTimer = setInterval(countdownStart, 1000);
+
+function countdownStart() {
+  if (timeStart) {
+    timer--;
+  } else if (timer <= 0) {
+    timer = 0;
+  }
+  timerSpan.innerHTML = timer;
+}
+
+// Start button eventlistener
+startBtn.addEventListener("click", function () {
+  homeDiv.remove();
+  quizDiv.style.display = "flex";
+  countdownStart();
+  timeStart = true;
+  showQuizQuestions();
+});
+
+// Function to display quiz questions
+
+function showQuizQuestions() {
+  questionTitle.textContent = questionsArr[i].question;
+  answerOne.textContent = questionsArr[i].answerChoices[0];
+  answerTwo.textContent = questionsArr[i].answerChoices[1];
+  answerThree.textContent = questionsArr[i].answerChoices[2];
+  answerFour.textContent = questionsArr[i].answerChoices[3];
+}
+
+// Event listeners and function for button clicks
+
+answerOne.addEventListener("click", function (e) {
+  var correctAnswer = questionsArr[i].correctAnswer;
+  console.log("correctAnswer " + correctAnswer);
+});
+
+answerTwo.addEventListener("click", function (e) {
+  var correctAnswer = questionsArr[i].correctAnswer;
+  console.log("correctAnswer " + correctAnswer);
+});
+
+answerThree.addEventListener("click", function (e) {
+  var correctAnswer = questionsArr[i].correctAnswer;
+  console.log("correctAnswer " + correctAnswer);
+});
+
+answerFour.addEventListener("click", function (e) {
+  var correctAnswer = questionsArr[i].correctAnswer;
+  console.log("correctAnswer " + correctAnswer);
+});
